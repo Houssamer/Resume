49615589083c18f58dfcc8ccd0b76fd4124f4ba9
RIAD Houssame <riad.houssame.eleve@ehtp.ac.ma>
2021-08-07 15:54:03 +0100

    first commit

---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function HomeScreen() {
+    return (
+        <div className="homeScreen">
+            
+        </div>
+    )
+}
+
+export default HomeScreen;
